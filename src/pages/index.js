daaae320ece4df86405be7fe478ee0808499dfd5
William matera <wmatera@Williams-MBP.home>
2020-02-25 19:54:59 -0500

    adding comments for tasks

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,41 @@ import NavHeader from "../components/navbar"
 
 const IndexPage = () => (
   <NavHeader />
+
+
+  // @@TODO items :
+
+  // content sections - services / contact me / gallery
+  // services - static markup - combination of <ul> tags
+  // contact me - google map - salon info - contact info - message me functionality
+  // gallery 
+
+  // a content compoenont 
+  // a contact me specific section
+
+
+  // navbar stlying UI
+  // will use bootstrap 
+  // remove drop down 
+  // add instagram icon - need to script in fa icons (font awesome) for each page
+  // other tabs wille - home - services - contact - gallery - plus instagram / other social if wanted 
+  // center the tabs 
+  // add hover state stlying to tabs 
+  
+
+  // we need a logo - will need to be a .png
+  // decide on the landing view 
+  // we need image assets 
+  // we need design input from macey
+  // we need color scheme input from macey 
+
+  // add google analytics tags 
+  // need to set up an account with google for this
+
+  
+
+
+
 )
 
 export default IndexPage

eaf815c742f9e969ea36724a24d5fe97b337d7f5
Ryan Kelly <ryankelly@Ryans-MBP.home>
2020-02-29 14:33:27 -0500

    worked on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,6 +8,19 @@ import "./layout.css"
 
 const NavHeader = () => {
 
+  // javascript for navbar hide on scroll
+
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
   return (
     <div>
     <Helmet>
@@ -16,24 +29,31 @@ const NavHeader = () => {
         
       {/* including font awesome incons */}
       <script src="https://kit.fontawesome.com/e2a027d1b1.js" crossorigin="anonymous"></script>
+
+      {/*importing bootstrap */}
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      
+      
     </Helmet>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand logo-icon" href="#">MM</a>
+    <nav id="navbar" class="navbar navbar-expand-md navbar-dark nav-div">
+        <a class="navbar-brand logo-icon nav-anchor" href="#">MM</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav navlist navpos">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
+            <li class="nav-item active nav-li-item nav-anchor">
+              <a class="nav-link nav-anchor" href="#">About<span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item nav-li-item">
               <a class="nav-link" href="profile.html">Services</a>
             </li>
-            <li class="nav-item dropdown">
+            <li class="nav-item dropdown nav-li-item">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
+                Gallery
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="#">Action</a>
@@ -42,11 +62,21 @@ const NavHeader = () => {
                 <a class="dropdown-item" href="#">Something else here</a>
               </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+            <li class="nav-item nav-li-item">
+            <a class="nav-link" href="profile.html">Contact</a>
+            </li>
+            <li class="nav-item nav-li-item">
+            <a class="nav-link" href="http://instagram.com/manesbymaceymorgan">
+              <div><span class="fab fa-instagram fa-lg" aria-hidden="true"></span></div>
+            </a>
+            </li>
+            <li class="nav-item nav-li-item">
+            <a class="nav-link" href="http://facebook.com/manesbymaceymorgan">
+              <div><span class="fab fa-facebook fa-lg" aria-hidden="true"></span></div>
+            </a>
             </li>
           </ul>
-          
+         
       </div>
       </nav>
       </div>

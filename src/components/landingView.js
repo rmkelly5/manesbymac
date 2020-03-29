92c65fbb5e9a8a93291fa472c8b5e3d9388c5610
William matera <matera.3@osu.edu>
2020-03-29 17:53:10 -0400

    changes

--- src/components/landingView.js
+++ src/components/landingView.js
@@ -13,6 +13,7 @@ const LandingView = ({ }) => {
   //const $ = typeof window !== `undefined` ? require("jquery") : null
 
   if (typeof window !== `undefined`) {
+
   $( document ).ready(function() { 
     var vid = document.getElementById("vid");
     vid.addEventListener("timeupdate", function () {
@@ -20,13 +21,26 @@ const LandingView = ({ }) => {
             this.currentTime = 0.0;
         }
     });
+
+    var banner = document.querySelector('.banner');
+    var bannerVideo = document.querySelector('.banner__video');
+
+    if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+      banner.style.backgroundImage = 'url("' + bannerVideo.poster + '")';
+      banner.style.backgroundSize = 'cover';
+      banner.style.backgroundPosition = 'center';
+      bannerVideo.style.display = 'none';
+    }
+
   });
+
+  
 }
   
   
   return (
     
-    <div style={{ height : '600px'}}>
+    <div style={{ height : '600px'}} className="banner">
       <h2 style={{ zIndex : '2' , position : 'relative' , paddingTop : '150px' , fontFamily : " 'Pacifico', cursive " 
                     , textAlign : 'center' , fontSize : '75px' , textShadow : 'rgba(0, 0, 0, 0.3) -10px 10px 18px, rgba(192, 128, 255, 0.1) 10px -10px 30px'}} >MM</h2>
 
@@ -36,7 +50,7 @@ const LandingView = ({ }) => {
       <h2 style={{ zIndex : '2' , position : 'relative' , paddingTop : '35px' , fontFamily : " 'Quicksand', sans-serif" 
                     , textAlign : 'center' , fontSize : '18px'}} >Hair Artist Based in Central New Jersey</h2>    
 
-      <video playsinline className="vidtest2" id="vid" preload autoPlay loop muted  >
+      <video className="vidtest2" id="vid" preload muted="muted" autoplay="autoplay" playsinline="playsinline" loop="loop"  >
         <source src={testvid} type="video/mp4"></source>
       </video> 
     </div>
